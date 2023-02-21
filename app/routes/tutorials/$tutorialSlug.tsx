5aef77b151b5f652cd3eda27d9a466c26e122d70
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-02-21 23:38:46 +0100

    feat: handle expect and unexpected error at the route level

--- app/routes/tutorials/$tutorialSlug.tsx
+++ app/routes/tutorials/$tutorialSlug.tsx
@@ -3,6 +3,7 @@ import { json, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { contentfulClient } from "~/utils/contentful.server";
 import { marked } from 'marked';
+import { Link, useCatch, useParams } from "@remix-run/react";
 import sanitizeHtml from 'sanitize-html';
 
 import type { Tutorial } from "~/types/types";
@@ -28,10 +29,42 @@ export default function Index() {
   const { tutorial } = useLoaderData<{ tutorial: Tutorial }>();
   return (
     <main className="container mx-auto p-4">
-      <div className="prose prose-slate max-w-6xl bg-white mx-auto p-8 rounded mt-16 ">
+      <div className="prose prose-amber max-w-6xl bg-white mx-auto p-8 rounded mt-16 ">
         <h1>Blog: {tutorial.fields.title}</h1>
         <div className="mt-4" dangerouslySetInnerHTML={{ __html: sanitizeHtml(marked(tutorial.fields.content || '')) }} />
       </div>
     </main>
   )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  if (caught.status === 404) {
+    return (
+      <div className="container mx-auto p-4">
+        <div className="prose prose-amber max-w-6xl bg-white mx-auto p-8 rounded mt-16 ">
+          <h1>Page not found</h1>
+          <p>Sorry, we couldn’t find the page you’re looking for.</p>
+          <Link to="/" className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600">Go home</Link>
+        </div>
+      </div>
+    )
+  }
+  throw new Error(`Something went wrong: ${caught.status}`);
+}
+export function ErrorBoundary() {
+  const { tutorialSlug } = useParams();
+  return (
+
+    <div className="rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="ml-3">
+          <h1 className="text-sm font-medium text-red-800">Oups</h1>
+          <div className="mt-2 text-sm text-red-700">
+            {`There was an error loading the tutorial page by the slug ${tutorialSlug}. Sorry.`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
