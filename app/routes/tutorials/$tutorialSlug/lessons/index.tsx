c65f5c3cc999ceda00bd833aba4d3965e4bc8d60
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-02-23 10:30:55 +0100

    feat: Route setup

---
+++ app/routes/tutorials/$tutorialSlug/lessons/index.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "@remix-run/react";
+
+export default function Lessons() {
+
+  return (
+    <div className="flex h-full min-h-screen flex-grow flex-col">
+      <h1>Lesson</h1>
+    </div>
+  )
+}
