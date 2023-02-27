b87159fd6d61522f698c953ecd86802c8e931ee7
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-02-27 10:08:56 +0100

    chore: Add clsx

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
   useCatch,
   useLoaderData,
 } from '@remix-run/react'
+import clsx from 'clsx'
 import Footer from '~/components/footer'
 import stylesheet from '~/styles/tailwind.css'
 import { getUser } from './utils/session.server'
@@ -48,60 +49,60 @@ function Document({
   title: string
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
         <title>{title}</title>
       </head>
-      <body className="bg-slate-900 text-white flex flex-col min-h-screen relative">
+      <body className='bg-slate-900 text-white flex flex-col min-h-screen relative'>
         <svg
-          className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-          aria-hidden="true"
+          className='absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+          aria-hidden='true'
         >
           <defs>
             <pattern
-              id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+              id='983e3e4c-de6d-4c3f-8d64-b9761d1534cc'
               width={200}
               height={200}
-              x="50%"
+              x='50%'
               y={-1}
-              patternUnits="userSpaceOnUse"
+              patternUnits='userSpaceOnUse'
             >
-              <path d="M.5 200V.5H200" fill="none" />
+              <path d='M.5 200V.5H200' fill='none' />
             </pattern>
           </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <svg x='50%' y={-1} className='overflow-visible fill-gray-800/20'>
             <path
-              d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+              d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
               strokeWidth={0}
             />
           </svg>
           <rect
-            width="100%"
-            height="100%"
+            width='100%'
+            height='100%'
             strokeWidth={0}
-            fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+            fill='url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)'
           />
         </svg>
-        <div className="bg-slate-900">
-          <nav className="container mx-auto flex justify-between items-center">
-            <ul className="flex">
-              <li className="p-4">
+        <div className='bg-slate-900'>
+          <nav className='container mx-auto flex justify-between items-center'>
+            <ul className='flex'>
+              <li className='p-4'>
                 <NavLink
                   className={({ isActive }) =>
-                    `${
+                    clsx(
                       isActive
                         ? 'border-b border-b-amber-500 text-amber-600'
-                        : 'text-white'
-                    }`
+                        : 'text-white',
+                    )
                   }
-                  to="/"
+                  to='/'
                 >
                   Rubber goose
                 </NavLink>
               </li>
-              <li className="p-4">
+              <li className='p-4'>
                 <NavLink
                   className={({ isActive }) =>
                     `${
@@ -110,12 +111,12 @@ function Document({
                         : 'text-white'
                     }`
                   }
-                  to="/tutorials"
+                  to='/tutorials'
                 >
                   Tutorials
                 </NavLink>
               </li>
-              <li className="p-4">
+              <li className='p-4'>
                 <NavLink
                   className={({ isActive }) =>
                     `${
@@ -124,12 +125,12 @@ function Document({
                         : 'text-white'
                     }`
                   }
-                  to="/tips"
+                  to='/tips'
                 >
                   Tips
                 </NavLink>
               </li>
-              <li className="p-4">
+              <li className='p-4'>
                 <NavLink
                   className={({ isActive }) =>
                     `${
@@ -138,28 +139,28 @@ function Document({
                         : 'text-white'
                     }`
                   }
-                  to="/blog"
+                  to='/blog'
                 >
                   Blog
                 </NavLink>
               </li>
             </ul>
-            <ul className="flex">
-              <li className="p-4">
+            <ul className='flex'>
+              <li className='p-4'>
                 {user ? (
-                  <div className="flex justify-between space-x-4 items-center">
+                  <div className='flex justify-between space-x-4 items-center'>
                     <span>{`Hi ${user.username}`}</span>
-                    <form action="/logout" method="post">
+                    <form action='/logout' method='post'>
                       <button
-                        type="submit"
-                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-amber-600 py-2 px-4 text-sm font-medium text-white hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
+                        type='submit'
+                        className='group relative flex w-full justify-center rounded-md border border-transparent bg-amber-600 py-2 px-4 text-sm font-medium text-white hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2'
                       >
                         Logout
                       </button>
                     </form>
                   </div>
                 ) : (
-                  <NavLink to="/login">Login</NavLink>
+                  <NavLink to='/login'>Login</NavLink>
                 )}
               </li>
             </ul>
@@ -176,7 +177,7 @@ function Document({
 export default function App() {
   const { user } = useLoaderData<typeof loader>()
   return (
-    <Document user={user} title="Rubbergoose">
+    <Document user={user} title='Rubbergoose'>
       <Outlet />
     </Document>
   )
@@ -186,10 +187,10 @@ export function CatchBoundary() {
 
   return (
     <Document user={null} title={`${caught.status} ${caught.statusText}`}>
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="ml-3">
-            <h1 className="text-sm font-medium text-red-800">
+      <div className='rounded-md bg-red-50 p-4'>
+        <div className='flex'>
+          <div className='ml-3'>
+            <h1 className='text-sm font-medium text-red-800'>
               {caught.status} {caught.statusText}
             </h1>
           </div>
@@ -200,12 +201,12 @@ export function CatchBoundary() {
 }
 export function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <Document user={null} title="Uh-oh!">
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="ml-3">
-            <h1 className="text-sm font-medium text-red-800">App Error</h1>
-            <div className="mt-2 text-sm text-red-700">
+    <Document user={null} title='Uh-oh!'>
+      <div className='rounded-md bg-red-50 p-4'>
+        <div className='flex'>
+          <div className='ml-3'>
+            <h1 className='text-sm font-medium text-red-800'>App Error</h1>
+            <div className='mt-2 text-sm text-red-700'>
               <pre>{error.message}</pre>
             </div>
           </div>
