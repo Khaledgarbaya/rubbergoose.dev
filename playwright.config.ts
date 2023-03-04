fb1071977a6c5f9b323b9f96dcf980795b0b3ec9
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-03-04 15:03:20 +0100

    fix: make sure docker and prisma are up before running tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
      */
     timeout: 5000,
   },
-  /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
